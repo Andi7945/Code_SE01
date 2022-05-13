0b5e0a9ce939d61d714cabf5c83a7a2b0e8ccd9e
Andi7945 <haering.andi@gmail.com>
2022-05-13 17:57:59 +0200

    have of Task 4

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/andreasharing/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/andreasharing/StudioProjects/Code_SE01"
+export "FLUTTER_APPLICATION_PATH=/Users/andreasharing/Documents/Projekte/CODE-Projects/Code_SE01"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/games/LightsOutAdvanced.dart
+++ lib/games/LightsOutAdvanced.dart
@@ -18,9 +18,18 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
     2 : Colors.blue,
   };
 
+  int grit_x = 3;
+  int grid_Y = 3;
 
   List<List<int>> lights = [];
   String msg = "";
+  int counter = 0;
+
+  //first: interation //second: x && y
+  List<List<int>> winnerPath = [];
+
+
+  bool exitCalculation = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,23 +53,9 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
                       children: List<int>.generate(lights[firstDigit].length, (i) => i).map((secondDigit) =>
                           GestureDetector(
                             onTap: (){
-                              lights[firstDigit][secondDigit] = incrementColor(actColor: lights[firstDigit][secondDigit]);
+                              counter++;
+                              lights = tabOnOneLight(lights: lights, firstDigit: firstDigit, secondDigit: secondDigit);
 
-                              if(firstDigit != 0){
-                                lights[firstDigit-1][secondDigit] = incrementColor(actColor: lights[firstDigit-1][secondDigit]);
-                              }
-                              if(secondDigit != 0){
-                                lights[firstDigit][secondDigit-1] = incrementColor(actColor: lights[firstDigit][secondDigit-1]);
-                              }
-
-                              if(firstDigit != lights.length-1){
-                                lights[firstDigit+1][secondDigit] = incrementColor(actColor: lights[firstDigit+1][secondDigit]);
-                              }
-                              if(secondDigit != lights.first.length-1){
-                                lights[firstDigit][secondDigit+1] = incrementColor(actColor: lights[firstDigit][secondDigit+1]);
-                              }
-
-                              checkIfUserWonTheGame();
                               setState(() {});
                             },
                             child: Padding(
@@ -72,7 +67,7 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
                                 decoration: BoxDecoration(
                                   color: colorMap[lights[firstDigit][secondDigit]],
                                   border: Border.all(
-                                      color: Colors.black,
+                                      color: isWinnterPath(count: counter, x: firstDigit, y: secondDigit) ? Colors.orange : Colors.black ,
                                       width: 1),
                                   borderRadius:
                                   const BorderRadius.all(
@@ -95,18 +90,72 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
                   msg = "";
                 });
 
-              }, child: const Text("Reset Game"))
+              }, child: const Text("Reset Game")),
+              const SizedBox(height: 20,),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  resetLightsDoable(level: 1);
+                  msg = "";
+                });
+
+              }, child: const Text("Reset Game doable")),
+              // const SizedBox(height: 20,),
+              // ElevatedButton(onPressed: (){
+              //   setState(() {
+              //     counter = 0;
+              //     winnerPath = testIfDoable(depth: 3);
+              //     msg = "";
+              //   });
+              //
+              // }, child: const Text("Test if game is duable")),
+
+              const SizedBox(height: 20,),
             ],
           ),
         )
     );
   }
+  bool isWinnterPath({required count, required x, required y}){
+    if(winnerPath.isEmpty) return false;
+    if(winnerPath[counter][1] == x && winnerPath[counter][2] == y) return true;
+    return false;
+  }
+
+  // List<List<List<int>>> testIfDoable({required depth}){
+  //   final List<List<int>> storeLigts = lights;
+  //   List<List<List<List<int>>> lightTests = [];
+  //   for(int z = 0; z < depth; z++){
+  //     lightTests.add([]);
+  //     for(int x = 0; x < storeLigts.length; x++) {
+  //       for (int y = 0; y < storeLigts.first.length; y++) {
+  //         lightTests.last(tabOnOneLight(lights: lightTests.last, firstDigit:x, secondDigit:y));
+  //         if(checkIfUserWonTheGame(lights: lightTests.last)){
+  //           List<List<int>> returnValue = [];
+  //           for(int i = 0; i < lightTests[z].length; i++){
+  //             returnValue.add(lightTests[z]);
+  //           }
+  //           return returnValue;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return [];
+  // }
 
 
+  void resetLightsDoable({required level}){
+    Random b = Random();
+    final int c =  b.nextInt(3);
+    lights = List.generate(grit_x, (index) => List.generate(grid_Y, (index) => c));
+    for(int x = 0; x < level; x++){
+      Random r = Random();
+      lights = tabOnOneLightBackwards(lights: lights, firstDigit: r.nextInt(lights.length), secondDigit: r.nextInt(lights.first.length));
+    }
+  }
 
   void resetLights(){
     Random r = Random();
-    lights = List.generate(5, (index) => List.generate(5, (index) => r.nextInt(3)));
+    lights = List.generate(grit_x, (index) => List.generate(grid_Y, (index) => r.nextInt(3)));
   }
 
   int incrementColor({required int actColor}){
@@ -115,12 +164,18 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
     return returnValue;
   }
 
-  void checkIfUserWonTheGame({required }){
+  int decrementColor({required int actColor}){
+    int returnValue = actColor - 1;
+    if(returnValue < 0) returnValue = 2;
+    return returnValue;
+  }
+
+  bool checkIfUserWonTheGame({required lights}){
     bool allLightsOff = true;
-    int lastColor = lights.first.first;
+    int firstColor = lights.first.first;
     for(int x = 0; x < lights.length; x++){
       for(int y = 0; y < lights[x].length; y++){
-        if(lights[x][y] == lastColor) {
+        if(lights[x][y] != firstColor) {
           allLightsOff = false;
           break;
         }
@@ -129,7 +184,47 @@ class _LightsOutAdvancedState extends State<LightsOutAdvanced> {
     }
     if(allLightsOff){
       msg = "You have won :)";
+      return true;
     }
+    return false;
+  }
+
+  List<List<int>> tabOnOneLight({required lights, required firstDigit, required secondDigit}){
+    lights[firstDigit][secondDigit] = incrementColor(actColor: lights[firstDigit][secondDigit]);
+
+    if(firstDigit != 0){
+      lights[firstDigit-1][secondDigit] = incrementColor(actColor: lights[firstDigit-1][secondDigit]);
+    }
+    if(secondDigit != 0){
+      lights[firstDigit][secondDigit-1] = incrementColor(actColor: lights[firstDigit][secondDigit-1]);
+    }
+
+    if(firstDigit != lights.length-1){
+      lights[firstDigit+1][secondDigit] = incrementColor(actColor: lights[firstDigit+1][secondDigit]);
+    }
+    if(secondDigit != lights.first.length-1){
+      lights[firstDigit][secondDigit+1] = incrementColor(actColor: lights[firstDigit][secondDigit+1]);
+    }
+    return lights;
+  }
+
+  List<List<int>> tabOnOneLightBackwards({required lights ,required firstDigit, required secondDigit}){
+    lights[firstDigit][secondDigit] = decrementColor(actColor: lights[firstDigit][secondDigit]);
+
+    if(firstDigit != 0){
+      lights[firstDigit-1][secondDigit] = decrementColor(actColor: lights[firstDigit-1][secondDigit]);
+    }
+    if(secondDigit != 0){
+      lights[firstDigit][secondDigit-1] = decrementColor(actColor: lights[firstDigit][secondDigit-1]);
+    }
+
+    if(firstDigit != lights.length-1){
+      lights[firstDigit+1][secondDigit] = decrementColor(actColor: lights[firstDigit+1][secondDigit]);
+    }
+    if(secondDigit != lights.first.length-1){
+      lights[firstDigit][secondDigit+1] = decrementColor(actColor: lights[firstDigit][secondDigit+1]);
+    }
+    return lights;
   }
 
 }
